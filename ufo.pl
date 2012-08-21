631c243730491c8f6e3be0196931c2f1e61818e5
jkline <jkline@evil.(none)>
2012-08-21 11:05:43 -0400

    added declaration of $parser

--- ufo.pl
+++ ufo.pl
@@ -10,8 +10,10 @@ use Text::CSV::Simple;
 
 my $base = q(/home/jkline/b/books/oreilly/machine-learning-files/johnmyleswhite-ML_for_Hackers-693be94/);
 my $datafile = qq($base/01-Introduction/data/ufo/ufo_awesome.tsv);
+
+my $parser = Text::CSV::Simple->new();
 my @data = $parser->read_file($datafile);
 
 foreach my $line ( @data) {
-  say join "," @$line;
+  say join ",",@$line;
 }
