ad12a5f5a84c8fedcfe270191a4abd05867c7181
jkline <jkline@evil.(none)>
2012-08-21 11:03:35 -0400

    added base stuff import/parse/display data

---
+++ ufo.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings
+use autodie;
+
+use v5.16;
+
+use Text::CSV::Simple;
+
+my $base = q(/home/jkline/b/books/oreilly/machine-learning-files/johnmyleswhite-ML_for_Hackers-693be94/);
+my $datafile = qq($base/01-Introduction/data/ufo/ufo_awesome.tsv);
+my @data = $parser->read_file($datafile);
+
+foreach my $line ( @data) {
+  say join "," @$line;
+}
